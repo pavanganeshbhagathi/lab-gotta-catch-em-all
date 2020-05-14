8fc85a689e6a1476148e16dcc65947c788e574ad
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-05-14 11:07:04 +0530

    last

--- src/model/Bulbasaur.java
+++ src/model/Bulbasaur.java
@@ -19,5 +19,61 @@ package model;
 //evolution = "IVYSAUR"
 //baseExp = "64" 
 
+public class Bulbasaur  extends Pokemon{
+	
+	
+	String characteristics;
+	String type;
+	String evolution;
+	String baseExp;
+	
+	public Bulbasaur(String characteristics,String type,String evolution, String baseExp,String pokemonName,int pokemonNumber)
+	{
+		
+		super(pokemonName,pokemonNumber);
+		this.characteristics=characteristics;
+		this.type=type;
+		this.baseExp=baseExp;
+	}
+
+	public Bulbasaur(String pokemonName, int pokemonNumber) {
+		super(pokemonName, pokemonNumber);
+	}
+
+	public String getCharacteristics() {
+		return characteristics;
+	}
+
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getEvolution() {
+		return evolution;
+	}
+
+	public void setEvolution(String evolution) {
+		this.evolution = evolution;
+	}
+
+	public String getBaseExp() {
+		return baseExp;
+	}
+
+	public void setBaseExp(String baseExp) {
+		this.baseExp = baseExp;
+	}
 
 
+	
+	
+	
+}
